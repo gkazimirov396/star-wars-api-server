bbde7c98dc282289c673cf41c8f8e7da744c8764
gkazimirov396 <grishakazim@gmail.com>
2024-12-16 12:22:09 +0200

    Fixed fetchMovies to pass the test

--- utils/fetchMovies.js
+++ utils/fetchMovies.js
@@ -5,7 +5,7 @@ export const fetchMovies = async (...urls) => {
 
     const movies = await Promise.all(results.map(res => res.json()));
 
-    return movies.map(film => film.result.properties);
+    return movies.map(film => film?.result?.properties);
   } catch (error) {
     console.error('Failed to fetch movies: ', error);
   }
